d09812515fa7c626c89101b5368b3ad032a1cdde
Guillian Vibert <guillianvibert@MacBook-de-Guillian.local>
2023-05-11 00:40:10 +0200

    feat: add unsplash error

--- src/routes/unsplash/random/+page.server.js
+++ src/routes/unsplash/random/+page.server.js
@@ -1,5 +1,5 @@
 import { UNSPLASH_API_KEY } from '$env/static/private'
-
+import { error,redirect } from '@sveltejs/kit';
 export const load = async (serverloadEvent) =>{
     const { fetch } = serverloadEvent;
     const response = await fetch('https://api.unsplash.com/photos/random',{
@@ -9,7 +9,7 @@ export const load = async (serverloadEvent) =>{
         }
     });
     if(!response.ok)
-        throw new Error(`Error: ${response.status} ${response.statusText}`);
+        throw error(response.status,{message:`Error: ${response.status} ${response.statusText}`});
 
     let photo = await response.json();
     return photo
