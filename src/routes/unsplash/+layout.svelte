6400baeab181ebfe872f41ede141c847705134d8
Guillian Vibert <guillianvibert@MacBook-de-Guillian.local>
2023-05-12 00:47:27 +0200

    feat: add svelte head and get child page data

--- src/routes/unsplash/+layout.svelte
+++ src/routes/unsplash/+layout.svelte
@@ -8,23 +8,4 @@
     <title>{$page.data.title || 'Selte Begins'}</title>
 </svelte:head>
 
-<div class="app">
-
-    LE giga layout
-	<main>
-		<slot />
-	</main>
-
-</div>
-
-<style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-        background-color: antiquewhite;
-	}
-
-</style>
-
-
+<slot />
--- src/routes/unsplash/random/+page.js
+++ src/routes/unsplash/random/+page.js
@@ -3,6 +3,8 @@ import PhotoComponent from '../photo.component.svelte';
 export const load = async (loadEvent) =>{
     const { data } = loadEvent;
     return {
+        title: data.alt_description,
+        icon : data.urls.thumb,
         randomPhoto : {
                 photoData: data,
                 Component: PhotoComponent
