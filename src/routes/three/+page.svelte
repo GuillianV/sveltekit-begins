c9b4089bd7be231d86ccc21b7b97b8e736f3e86d
Guillian VIBERT-VALLET <guillian.vibert-vallet@boondooa.com>
2023-05-11 13:50:03 +0200

    feat:add threejs

---
+++ src/routes/three/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+	import { onMount } from 'svelte';
+    import { CubeScene } from '$lib/three/cube.scene.js';
+
+
+	let el;
+
+	onMount(() => {
+        let cubeScene = new CubeScene({
+            width: window.innerWidth -40,
+            height: window.innerHeight-40
+        })
+        cubeScene.createScene(el)
+	});
+</script>
+
+<svelte:head>
+	<title>Three.js</title>
+	<meta name="description" content="Three.js example app built with Svelte" />
+</svelte:head>
+
+<canvas bind:this={el} />
+
