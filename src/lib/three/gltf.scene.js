3e0f2387bd7e33a9cd0f5043f8de89226a6143d5
Guillian Vibert-Vallet <guillian.vibert-vallet@boondooa.com>
2023-05-12 15:03:35 +0200

    feat : add gltf three

---
+++ src/lib/three/gltf.scene.js
@@ -0,0 +1,102 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+export class GLTFScene {
+
+	constructor(properties){
+
+		this.width = 100;
+		this.height = 100;
+
+
+		if(typeof properties === 'object'){
+			this.width = properties.width ?? this.width;
+			this.height = properties.height ?? this.height;
+		}
+	
+
+		if (typeof window == 'undefined')
+			return;
+		
+	
+		this.isDragging = false;
+		this.scene = new THREE.Scene();
+
+		this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+		this.camera.position.z = 5;
+	
+		this.geometry = new THREE.BoxGeometry();
+
+		this.pointLight = new THREE.PointLight(0x0000ff,0.2);
+		 this.pointLight.position.set(-10, 10, -10).normalize();
+		this.scene.add(this.pointLight);
+
+		this.renderer = null;
+	
+		this.starnight = null;
+		
+		self = this;
+		var loader = new GLTFLoader();
+		loader.load( '/models/switch/scene.gltf', function ( gltf )
+		{
+	
+			self.starnight = gltf.scene 
+			self.starnight.castShadow = true;
+			self.starnight.receiveShadow = true;
+
+			
+			self.scene.add(self.starnight);
+		});     
+
+
+		window.addEventListener('mousedown', this.onMouseDown.bind(this));
+		window.addEventListener('mouseup', this.onMouseUp.bind(this));
+		window.addEventListener('resize', this.resize);
+
+		window.addEventListener('mousemove', (event) => {
+			if (this.isDragging) {
+				const mouseX = event.clientX;
+				const mouseY = event.clientY;
+				this.starnight.rotation.y += (mouseX - this.width / 2) * 0.00005;
+				this.starnight.rotation.x += (mouseY - this.height / 2) * 0.00005;
+				this.renderer.render(this.scene, this.camera);
+			}
+		});
+	}
+
+	onMouseDown(event) {
+		this.isDragging = true;
+	}
+	  
+	onMouseUp(event) {
+		this.isDragging = false;
+	}
+
+	resize()  {
+		this.renderer.setSize(this.width, this.height);
+		this.camera.aspect = this.width / this.height;
+		this.camera.updateProjectionMatrix();
+	}
+
+	animate(){
+		
+		requestAnimationFrame(()=>{
+			this.animate()
+		});
+		if(!this.isDragging){
+			// this.starnight.rotation.x += 0.01;
+			// this.starnight.rotation.y += 0.01;
+			this.renderer.render(this.scene, this.camera);
+		}
+
+	}
+
+	createScene(el) {
+
+		this.renderer = new THREE.WebGLRenderer({  antialias: true, canvas: el ,alpha: true });
+		this.resize();
+		this.animate();
+
+	}
+
+}
---
+++ src/routes/three/gltf/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { onMount } from 'svelte';
+    import { GLTFScene } from '$lib/three/gltf.scene.js';
+
+
+	let el;
+
+	onMount(() => {
+        let gltfScene = new GLTFScene({
+            width: window.innerWidth -40,
+            height: window.innerHeight-40
+        })
+        gltfScene.createScene(el)
+	});
+</script>
+
+<svelte:head>
+	<title>Three.js</title>
+	<meta name="description" content="Three.js gltf app built with Svelte" />
+</svelte:head>
+
+<h1>GLTF</h1>
+
+<canvas bind:this={el} />
+
