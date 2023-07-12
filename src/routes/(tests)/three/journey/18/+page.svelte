7fadc7693000d73a6452f8ecd78ee7cbd32b5a87
Guillian Vibert-Vallet <guillian.vibert-vallet@boondooa.com>
2023-07-12 17:04:09 +0200

    feat: 18 particles

--- src/routes/(tests)/three/journey/18/+page.svelte
+++ src/routes/(tests)/three/journey/18/+page.svelte
@@ -8,9 +8,8 @@
     import GUI from 'lil-gui/dist/lil-gui.esm'; 
 
     onMount(() => {
+                                
                         
-                            
-                
         THREE.ColorManagement.enabled = false
 
         /**
@@ -29,43 +28,27 @@
          * Textures
          */
         const textureLoader = new THREE.TextureLoader()
+        const particleTexture = textureLoader.load("/three/journey/18/textures/particles/5.png")
+        particleTexture.magFilter = THREE.NearestFilter
 
         /**
-         * House
-         */
-        // Temporary sphere
-        const sphere = new THREE.Mesh(
-            new THREE.SphereGeometry(1, 32, 32),
-            new THREE.MeshStandardMaterial({ roughness: 0.7 })
-        )
-        sphere.position.y = 1
-        scene.add(sphere)
+         * Particles
+        */
+        const particlesGeometry = new THREE.BufferGeometry()
+        const particlesMaterial = new THREE.PointsMaterial({color:"#dadada",map:particleTexture,size:0.4,sizeAttenuation:true})
+        const particlesNumber = 5000;
+        const positions = new Float32Array(particlesNumber*3)
 
-        // Floor
-        const floor = new THREE.Mesh(
-            new THREE.PlaneGeometry(20, 20),
-            new THREE.MeshStandardMaterial({ color: '#a9c388' })
-        )
-        floor.rotation.x = - Math.PI * 0.5
-        floor.position.y = 0
-        scene.add(floor)
+        for(let i = 0; i < particlesNumber; i++){
+            positions[i] = (Math.random()- .5) *100
+        }
 
-        /**
-         * Lights
-         */
-        // Ambient light
-        const ambientLight = new THREE.AmbientLight('#ffffff', 0.5)
-        gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001)
-        scene.add(ambientLight)
+        particlesGeometry.setAttribute("position",new THREE.BufferAttribute(positions,3))
 
-        // Directional light
-        const moonLight = new THREE.DirectionalLight('#ffffff', 0.5)
-        moonLight.position.set(4, 5, - 2)
-        gui.add(moonLight, 'intensity').min(0).max(1).step(0.001)
-        gui.add(moonLight.position, 'x').min(- 5).max(5).step(0.001)
-        gui.add(moonLight.position, 'y').min(- 5).max(5).step(0.001)
-        gui.add(moonLight.position, 'z').min(- 5).max(5).step(0.001)
-        scene.add(moonLight)
+        const particles = new THREE.Points(particlesGeometry,particlesMaterial)
+        scene.add(particles)
+
+
 
         /**
          * Sizes
@@ -95,9 +78,7 @@
          */
         // Base camera
         const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-        camera.position.x = 4
-        camera.position.y = 2
-        camera.position.z = 5
+        camera.position.z = 3
         scene.add(camera)
 
         // Controls
